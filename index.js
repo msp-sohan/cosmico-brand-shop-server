7678a39628096e7a815ed2dfe5ef4e8e5a6f2628
mspsohan <mspsohan@gmail.com>
2023-10-20 20:19:59 +0600

    Delete await from client connect to slove the not found product problem

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const client = new MongoClient(uri, {
 async function run() {
    try {
       // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+      // await client.connect();
+      client.connect();
 
       const productCollection = client.db('CosmicoDB').collection('Product')
       const brandCollection = client.db('CosmicoDB').collection('Brand')
