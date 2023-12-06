9c2783d3e6aca92ffe9cfabc4ab8e0d24011e180
mspsohan <mspsohan@gmail.com>
2023-12-07 05:25:32 +0600

    Impletement Stripe Payment Gateway

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-app.use(cors())
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
+app.use(cors({
+   origin: ['http://localhost:5173']
+}))
 app.use(express.json())
 
 // const uri = "mongodb://localhost:27017"
@@ -32,66 +36,67 @@ async function run() {
       const testimonials = client.db('CosmicoDB').collection('Testimonial')
       const cartCollection = client.db('CosmicoDB').collection('MyCart')
 
-      app.get('/products', async(req, res)=>{
+      app.get('/products', async (req, res) => {
          const cursor = productCollection.find()
          const result = await cursor.toArray()
          res.send(result)
       })
 
-      app.get('/products/brands', async(req, res)=>{
+      app.get('/products/brands', async (req, res) => {
          const cursor = brandCollection.find()
          const result = await cursor.toArray()
          res.send(result)
       })
-      app.get('/advertisement', async(req, res)=>{
+      app.get('/advertisement', async (req, res) => {
          const cursor = advertisement.find()
          const result = await cursor.toArray()
          res.send(result)
       })
 
-      app.get('/products/:id', async(req, res)=>{
+      app.get('/products/:id', async (req, res) => {
          const id = req.params.id
-         const query = {_id: new ObjectId(id)}
+         console.log(id)
+         const query = { _id: new ObjectId(id) }
          const product = await productCollection.findOne(query)
          res.send(product)
       })
 
-      app.get('/testimonial', async(req, res)=>{
+      app.get('/testimonial', async (req, res) => {
          const cursor = testimonials.find()
          const result = await cursor.toArray()
          res.send(result)
       })
 
-      app.get('/mycart', async(req, res)=>{
-         const cursor = cartCollection.find()
-         const result = await cursor.toArray()
+      app.get('/mycart/:email', async (req, res) => {
+         const email = req.params.email
+         const result = await cartCollection.find({ email: email }).toArray()
          res.send(result)
       })
 
-      app.get('/mycart/:id', async(req, res)=>{
-         const id = req.params.id
-         const query = {_id: new ObjectId(id)}
-         const result = await cartCollection.findOne(query)
-         res.send(result)
-      })
+      // app.get('/mycart/:id', async (req, res) => {
+      //    const id = req.params.id
+      //    const query = { _id: new ObjectId(id) }
+      //    const result = await cartCollection.findOne(query)
+      //    res.send(result)
+      // })
 
-      app.post('/products', async(req, res)=>{
+      app.post('/products', async (req, res) => {
          const addProduct = req.body;
          const result = await productCollection.insertOne(addProduct);
          res.send(result)
       })
 
-      app.post('/mycart', async(req, res)=>{
+      app.post('/mycart', async (req, res) => {
          const myCart = req.body
          const result = await cartCollection.insertOne(myCart)
          res.send(result)
       })
 
-      app.put('/products/:id', async(req, res)=>{
+      app.put('/products/:id', async (req, res) => {
          const id = req.params.id
          const product = req.body
-         const filter = {_id: new ObjectId(id)}
-         const options = {upsert: true}
+         const filter = { _id: new ObjectId(id) }
+         const options = { upsert: true }
          const updateProduct = {
             $set: {
                name: product.name,
@@ -106,12 +111,35 @@ async function run() {
          res.send(result)
       })
 
-      app.delete('/mycart/:id', async(req, res)=>{
+      app.delete('/products/:id', async (req, res) => {
          const id = req.params.id;
-         const query = { _id: new ObjectId(id)}
+         console.log(id)
+         const query = { _id: new ObjectId(id) }
+         const result = await productCollection.deleteOne(query);
+         res.send(result);
+      })
+
+      app.delete('/mycart/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
          const result = await cartCollection.deleteOne(query);
          res.send(result);
       })
+
+      app.post("/create-payment-intent", async (req, res) => {
+         const { price } = req.body;
+         const amount = parseInt(price * 100);
+         if (!price || amount < 1) return;
+         // Create a PaymentIntent with the order amount and currency
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "usd",
+            payment_method_types: ['card'],
+         });
+         res.send({
+            clientSecret: paymentIntent.client_secret,
+         });
+      });
 
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
